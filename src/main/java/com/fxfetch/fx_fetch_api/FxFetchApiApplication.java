eb7aded6a9d4c7703739b02fbaa30055fe8596c5
Carlos-Cao <carlos.cao2@gmail.com>
2025-10-06 21:28:24 +1300

    chore: initialise with template Spring Boot structure

---
+++ src/main/java/com/fxfetch/fx_fetch_api/FxFetchApiApplication.java
@@ -0,0 +1,13 @@
+package com.fxfetch.fx_fetch_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxFetchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxFetchApiApplication.class, args);
+	}
+
+}
